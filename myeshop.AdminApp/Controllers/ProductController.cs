e75aad0dcc10d3756b6294324f3f4fcb2485773c
knhuji <kimnhu10tn1@gmail.com>
2020-06-10 23:42:18 +0700

    Detail product

--- myeshop.AdminApp/Controllers/ProductController.cs
+++ myeshop.AdminApp/Controllers/ProductController.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using myeshop.Data.EF;
 
 namespace myeshop.AdminApp.Controllers
 {
     public class ProductController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details()
         {
             return View();
         }
