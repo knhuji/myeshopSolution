4150eb43259cd0f8e6c8a19689d5a38452191efd
phuong-mai <55681843+phuong-mai@users.noreply.github.com>
2020-06-11 23:42:40 +0700

    thay đổi

---
+++ myeshop.Application/Catalog/Carts/ICartService.cs
@@ -0,0 +1,16 @@
+﻿using myeShop.ViewModels.Catalog.Carts;
+using myeShop.ViewModels.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myeshop.Application.Catalog.Carts
+{
+    public interface ICartService
+    {
+        Task<ApiResult<Cart>> GetById(String key);
+        Task<ApiResult<Cart>> Updatecart(String key, Cart request);
+        Task<ApiResult<bool>> DeleteCart(String key);
+    }
+}
